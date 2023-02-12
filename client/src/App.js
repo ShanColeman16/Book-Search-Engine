15e67d1078eba6d898814ff91bad6523e39b79f0
unknown <shannoncoleman16@gmail.com>
2023-02-12 16:07:55 -0500

    imported setcontext from apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
