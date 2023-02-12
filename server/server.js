bc0f84eb6fd6cbcbd543be04c512e002978e64c1
unknown <shannoncoleman16@gmail.com>
2023-02-11 23:18:09 -0500

    imported apolloserver class

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
