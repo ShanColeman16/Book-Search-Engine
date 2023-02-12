e64ae94a7c8c3c18a70d38e18118b5ac75ab0b40
unknown <shannoncoleman16@gmail.com>
2023-02-11 22:51:04 -0500

    setup deleteook for mutation function

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,47 +1,46 @@
-const { getSingleUser } = require('../controllers/user-controller');
 const { User } = require('../models');
 
 const resolvers = {
-  Query:  {
-   async getSingleUser(_parent, args, context) {
-    const { user } = context;
-    const foundUser = await User.findOne({
-      $or: [
-        { _id: user ? user._id : args.id }, 
-        { username: args.username }
-      ],
-    });
-    return foundUser;
-   },
+  Query: {
+    async getSingleUser(_parent, args, context) {
+      const { user } = context;
+      const foundUser = await User.findOne({
+        $or: [
+          { _id: user ? user._id : args.id },
+          { username: args.username }
+        ],
+      });
+      return foundUser;
+    },
   },
 
   Mutation: {
-    async createUser( _parent, args) {
-      const user = await User.create(body);
+    async createUser(_parent, args) {
+      const user = await User.create(args);
       const token = signToken(user);
 
-      return { user, token};
+      return { user, token };
     },
-    async login({ _parent }, email, password) {
-      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    async login(_parent, { email, password }) {
+      const user = await User.findOne({ email });
       if (!user) {
         return res.status(400).json({ message: "Can't find this user" });
       }
-  
-      const correctPw = await user.isCorrectPassword(body.password);
-  
+
+      const correctPw = await user.isCorrectPassword(password);
+
       if (!correctPw) {
         return res.status(400).json({ message: 'Wrong password!' });
       }
       const token = signToken(user);
       res.json({ token, user });
     },
-    async saveBook({ user, body }, res) {
+    async saveBook(_parent, { newBook }, context) {
       console.log(user);
       try {
         const updatedUser = await User.findOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: body } },
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: newBook } },
           { new: true, runValidators: true }
         );
         return res.json(updatedUser);
@@ -50,10 +49,17 @@ const resolvers = {
         return res.status(400).json(err);
       }
     },
-
-
-
-    
+    async deleteBook(_parent, { bookId }, context) {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
+    },
 
   }
 };
