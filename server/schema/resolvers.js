c749e52c5e6a62ed9fedc87e3dd5e5a830d9f595
unknown <shannoncoleman16@gmail.com>
2023-02-11 20:44:02 -0500

    inital commit

---
+++ server/schema/resolvers.js
@@ -0,0 +1,19 @@
+const { getSingleUser } = require('../controllers/user-controller');
+const { User } = require('../models');
+
+const resolvers = {
+  Query:  {
+   async getSingleUser(_parent, args, context) {
+    const { user } = context;
+    const foundUser = await User.findOne({
+      $or: [
+        { _id: user ? user._id : args.id }, 
+        { username: args.username }
+      ],
+    });
+    return foundUser;
+   },
+  }
+};
+
+module.exports = resolvers;
