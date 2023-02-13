2a061837aa91e227582f5590fd6f0f68be136d11
unknown <shannoncoleman16@gmail.com>
2023-02-12 20:22:04 -0500

    getting help

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const db = require('./config/connection');
 // Import the ApolloServer class
 const { ApolloServer } = require('apollo-server-express');
 // Import the two parts of a GraphQL schema
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schema');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
